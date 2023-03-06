8bf04d755274dc0f700b2e2fe0a21a71724c5a39
themausam <memausam@live.com>
2023-03-06 21:28:52 +0000

    Hello world from server

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OpenAIPlayground
+Nothing to see here. A playground to check out OpenAI offerings.
+- Backend Node.js 
+- Language TypeScript
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,14 @@
-var msg = "Hello World";
-console.log(msg);
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello Node\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}/${port}/`);
+});
